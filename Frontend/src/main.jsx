44de0610b61668769f00b5ac671faea573ed4d61
samuel-254-ke <wanjugusamuel254@gmail.com>
2024-10-07 13:08:22 +0300

    cart component

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -9,6 +9,7 @@ import Login from './Pages/login.jsx'
 import Registration from './Pages/Registration.jsx'
 import Shop from './Pages/shoppingPage.jsx'
 import ProductDetail from './Pages/productDetail.jsx';
+import CartPage from './Pages/cartPage.jsx'
 import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async';
 import {Provider} from 'react-redux';
@@ -23,6 +24,7 @@ const routes = createBrowserRouter( createRoutesFromElements(
   <Route path='/registration' element={<Registration />}/>
   <Route path='/shop' element={<Shop />}/>
   <Route path='/prd' element={<ProductDetail />} />
+  <Route path='/cart' element={<CartPage />} />
   </>
  
 ))
