b5fa941dd5adb7b7654a5b3f718fb36211be2b03
samuel-254-ke <wanjugusamuel254@gmail.com>
2024-10-07 11:38:12 +0300

    prd route

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -8,6 +8,7 @@ import Homepage from './Homepage/Homepage.jsx'
 import Login from './Pages/login.jsx'
 import Registration from './Pages/Registration.jsx'
 import Shop from './Pages/shoppingPage.jsx'
+import ProductDetail from './Pages/productDetail.jsx';
 import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async';
 import {Provider} from 'react-redux';
@@ -21,6 +22,7 @@ const routes = createBrowserRouter( createRoutesFromElements(
   <Route path='/login' element={<Login />}/>
   <Route path='/registration' element={<Registration />}/>
   <Route path='/shop' element={<Shop />}/>
+  <Route path='/prd' element={<ProductDetail />} />
   </>
  
 ))
