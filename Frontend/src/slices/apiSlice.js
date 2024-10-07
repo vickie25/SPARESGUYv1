1f0f945251c63d4eab590bb9d90a090d8bdde8eb
vindeacy <deacydavids70@gmail.com>
2024-10-07 09:58:17 +0300

    apislice resolved

--- Frontend/src/slices/apiSlice.js
+++ Frontend/src/slices/apiSlice.js
@@ -1,12 +1,14 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+// import { createApi } from '@reduxjs/toolkit/query/react';
 import {BASE_URL} from '../Constants/constants';
 
 const baseQuery = fetchBaseQuery({baseUrl: BASE_URL});
 
-export const apiSlice = createApi({
-    baseQuery,
-    tagTypes: ['users'],
-    endpoints:(builder) =>({
-        
-    })
-})
+const apiSlice = createApi({
+    reducerPath: 'api',
+    baseQuery: baseQuery,// Adjust base URL as needed
+    endpoints: (builder) => ({}), // Start with an empty object
+});
+
+export default apiSlice;
+
