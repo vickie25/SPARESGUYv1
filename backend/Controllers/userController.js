b5c2dfd38be1f58be9963edcdc49ad902f2db350
samuel-254-ke <wanjugusamuel254@gmail.com>
2024-10-07 14:58:11 +0300

    reviews

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -34,27 +34,34 @@ export const registerUser = async (req, res) => {
 
 export const loginUser = async (req, res) => {
     const { email, password } = req.body;
-    const user = await User.findOne({
-        email
-    }); 
-    if (!user) {
-        res.status(400).json({
-            message: "Invalid email or password"
-        });
-    }
-    const validPassword = await bcrypt.compare(password, user.password);
-    if (!validPassword) {
-        res.status(400).json({
-            message: "Invalid email or password"
-        });
-    }
 
-    const token = jwt.sign({
-        id: user._id
-    }, process.env.JWT_SECRET);
-    res.status(200).json({
-        token
-    });
-}
+    try {
+        // Find the user by email
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).json({ message: "Invalid email or password." });
+        }
+
+        // Compare the provided password with the hashed password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(401).json({ message: "Invalid email or password." });
+        }
+
+        // Generate a JWT token
+
+        console.log("JWT_SECRET:", process.env.JWT_SECRET);
+        const token = jwt.sign({ userId: user._id,  }, 'mySuperSecretKey123!', {
+            expiresIn: '1h',
+        });
+        console.log()
+        res.json({ token });
+
+
+    } catch (error) {
+        console.error("Login error:", error);
+        res.status(500).json({ message: "Internal server error." });
+    }
+};
 //get user profile
 
