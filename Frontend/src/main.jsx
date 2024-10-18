f6da11a31d19fa22321ca436f5725e4005668b65
LynnAkinyi <78434875+LynnAkinyi@users.noreply.github.com>
2024-10-18 15:27:11 +0300

    store.js

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -12,42 +12,42 @@ import ProductDetail from './Pages/productDetail.jsx';
 import CartPage from './Pages/cartPage.jsx'
 import Review from './Pages/Reviews.jsx'
 import UserProfile from './Pages/UserProfile.jsx'
-import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async';
-import {Provider} from 'react-redux';
-// import store from './store.js'
+import { Provider } from 'react-redux';
+import store from './store.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Checkout from './Pages/Checkout.jsx'
 import Payement from './Pages/Payement.jsx'
 import PaymentConfirmation from './Pages/PaymentConfirmation.jsx'
 
 
-const routes = createBrowserRouter( createRoutesFromElements(
+const routes = createBrowserRouter(createRoutesFromElements(
   <>
-  <Route path="/app" element={<App />} />
-  <Route index={true} path='/' element={<Homepage />}/>
-  <Route path='/login' element={<Login />}/>
-  <Route path='/registration' element={<Registration />}/>
-  <Route path='/shop' element={<Shop />}/>
-  <Route path='/details' element={<ProductDetail />} />
-  <Route path='/cart' element={<CartPage />} />
-  <Route path='/revew' element={<Review />} />
-  <Route path='/checkout' element={<Checkout />} />
-  <Route path='/payement' element={<Payement />} />
-  <Route path='/UserProf' element={<UserProfile />} />
-  <Route path='/confirmation' element={<PaymentConfirmation />} />
+    <Route path="/app" element={<App />} />
+    <Route index={true} path='/' element={<Homepage />} />
+    <Route path='/login' element={<Login />} />
+    <Route path='/registration' element={<Registration />} />
+    <Route path='/shop' element={<Shop />} />
+    <Route path='/details' element={<ProductDetail />} />
+    <Route path='/cart' element={<CartPage />} />
+    <Route path='/revew' element={<Review />} />
+    <Route path='/checkout' element={<Checkout />} />
+    <Route path='/payement' element={<Payement />} />
+    <Route path='/UserProf' element={<UserProfile />} />
+    <Route path='/confirmation' element={<PaymentConfirmation />} />
   </>
- 
+
 ))
 
 
 const root = ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <HelmetProvider>
-    <Provider store={store}>
-    <RouterProvider router={routes}/>
-    </Provider>
+    <HelmetProvider>
+      <Provider store={store}>
+        <RouterProvider router={routes} />
+      </Provider>
     </HelmetProvider>
   </React.StrictMode>
-  
+
 )
