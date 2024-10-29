3daaf911c28c37be2d8cc066e9c9979981bcf592
LynnAkinyi <78434875+LynnAkinyi@users.noreply.github.com>
2024-10-29 11:00:06 +0300

    header search, cart context

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { CartProvider } from './context/CartContext';  // Import the CartProvider
+import { CartProvider } from './context/CartContext';
+import { SearchProvider } from './context/SearchContext';  // Ensure correct import
 import { Outlet } from 'react-router-dom';
+import Header from './Homepage/Header';  // Import the Header component
 
 function App() {
   return (
     <CartProvider>
-      <Outlet />
+      <SearchProvider>
+        <Header />
+        <Outlet />
+      </SearchProvider>
     </CartProvider>
   );
 }
