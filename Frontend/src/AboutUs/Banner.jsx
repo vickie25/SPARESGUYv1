715dc6e3d8da3beedb075bf895a26292da060e49
LynnAkinyi <78434875+LynnAkinyi@users.noreply.github.com>
2024-11-08 14:58:52 +0300

    delete about us styles

--- Frontend/src/AboutUs/Banner.jsx
+++ Frontend/src/AboutUs/Banner.jsx
@@ -4,7 +4,7 @@ import bg from './AboutUsImages/bg.jpg';
 
 const Banner = () => {
   return (
-    <div className="Banner">
+    <div className="aboutBanner">
       <main
         style={{
           backgroundImage: `url(${bg})`,
@@ -14,12 +14,12 @@ const Banner = () => {
           filter: 'blur(8px)', // Apply blur effect
           position: 'relative', // Position relative for stacking
           zIndex: 0, // Background layer
-          
+
         }}
       >
       </main>
-      <div className="banner-content">
-        <div className="welcome">
+      <div className="bannerContent">
+        <div className="about-welcome">
           <h1 className="h">About Us At My Spares Guy</h1>
           <p className="p">
             At My Spares Guy, we combine expertise and passion to deliver reliable, high-quality car parts. Our mission is to keep you on the road with trusted solutions and exceptional service.
