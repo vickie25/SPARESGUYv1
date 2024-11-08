8462ec2a045f71dd123c7adeb545b43fb96cf5df
LynnAkinyi <78434875+LynnAkinyi@users.noreply.github.com>
2024-11-08 09:50:42 +0300

    products model ,modified

--- backend/Middleware/AuthMiddleware.js
+++ backend/Middleware/AuthMiddleware.js
@@ -1,28 +1,31 @@
 import jwt from 'jsonwebtoken';
-import dotenv from "dotenv"
-import User from "../Models/User.js";
+import dotenv from 'dotenv';
+import User from '../Models/User.js';
 
-dotenv.config()
+dotenv.config();
 
-const authMiddleware = async(req, res, next) => {
+const authMiddleware = async (req, res, next) => {
     const token = req.cookies.token;
-    console.log(token, "This is the token")
+    console.log(token, 'This is the token');
 
-    const key = "mySuperSecretKey123!";
-    console.log(key, "This isthe key")
+    const key = process.env.JWT_SECRET || 'mySuperSecretKey123!';
+    console.log(key, 'This is the key');
+
+    if (!token) {
+        return res.status(401).json({ message: 'Authentication error: Token not provided' });
+    }
 
     try {
-        const email = jwt.verify(token, key);
-        console.log(email, "This is the email")
-        const user = await User.findOne({ email: email.id });
-        console.log(user, "This is the user")
+        const decoded = jwt.verify(token, key);
+        console.log(decoded, 'This is the decoded token');
+        const user = await User.findOne({ email: decoded.email });
+        console.log(user, 'This is the user');
         req.user = user;
         next();
     } catch (error) {
-        console.error("Authentication error:", error);
-        res.status(401).json({ message: "Invalid token." });
+        console.error('Authentication error:', error);
+        res.status(401).json({ message: 'Invalid token.' });
     }
 };
 
 export default authMiddleware;
-
