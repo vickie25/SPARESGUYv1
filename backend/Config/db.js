45a979696b2e5a7c1589019395cda3a4612f1c6a
samuel-254-ke <wanjugusamuel254@gmail.com>
2025-01-15 12:34:13 +0300

    configdb changes

---
+++ backend/Config/db.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect("mongodb+srv://apbcafricait:apbcafricait@cluster0.tlkh7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+
+
+       
+
+        // await mongoose.connect("mongodb+srv://nanjalayvone:nanjalayvone@cluster0.wwaob.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+
+        console.log("MongoDB connected.");
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
