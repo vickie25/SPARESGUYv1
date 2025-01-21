9365d5ee7e5f3402469ae088cc51865939699fd2
vickie25 <nyandorovictor3900@gmail.com>
2025-01-21 12:13:34 +0300

    Your commit message

--- backend/Database config/db.js
+++ backend/Database config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/MYSPARESGUY");
+        await mongoose.connect("mongodb+srv://nanjalayvone:nanjalayvone@cluster0.wwaob.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
 
        
